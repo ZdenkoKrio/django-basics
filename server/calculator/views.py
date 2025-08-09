9f912e575bf0fb6e8f18b09cc7651fe2faca612b
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-08-09 16:57:08 +0200

    calc other operations

--- server/calculator/views.py
+++ server/calculator/views.py
@@ -6,10 +6,15 @@ from django.http import HttpResponse
 def calc_view(request):
     num1 = request.GET.get("num1")
     num2 = request.GET.get("num2")
+    operation = request.GET.get("operation")
 
     result = None
     if num1 is not None and num2 is not None:
-        result = int(num1) + int(num2)
+        if operation == "+":
+            result = int(num1) + int(num2)
+        
+        if operation == "-":
+            result = int(num1) - int(num2)
 
     return render(request, "calc.html", {"result": result})
 
