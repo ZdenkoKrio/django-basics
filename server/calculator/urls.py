805beff722a0423e73516b2a8325036270d78253
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-08-09 16:48:46 +0200

    calc plus

--- server/calculator/urls.py
+++ server/calculator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import calc_view
+from .views import calc_view, showcase_plus
 
 
 urlpatterns = [
     path('', calc_view, name="calc"),
+    path('show_p/<int:num1>/<int:num2>', showcase_plus),
 ]
--- server/calculator/views.py
+++ server/calculator/views.py
@@ -1,5 +1,18 @@
+from django.db.models.expressions import result
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def calc_view(request):
-    return render(request, "calc.html")
+    num1 = request.GET.get("num1")
+    num2 = request.GET.get("num2")
+
+    result = None
+    if num1 is not None and num2 is not None:
+        result = int(num1) + int(num2)
+
+    return render(request, "calc.html", {"result": result})
+
+
+def showcase_plus(request, num1, num2):
+    return HttpResponse(f"Vysledok: {num2+num1}")
