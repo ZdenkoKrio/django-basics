ac33e66f7d4303a9111eb6dcb4525b25895a75a9
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-08-10 16:02:48 +0200

    detail album view

--- server/music/urls.py
+++ server/music/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import band_list_view, band_detail_view, band_create_view
+from .views import (band_list_view, band_detail_view, band_create_view,
+                    album_detail_view)
 
 
 urlpatterns = [
     path('list/', band_list_view, name="bands"),
     path('detail/<int:id>', band_detail_view, name="b_detail"),
+    path('a_detail/<int:id>', album_detail_view, name="a_detail"),
     path('create/', band_create_view, name="b_create"),
 ]
--- server/music/views.py
+++ server/music/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect
-from .models import Band
+from .models import Band, Album, Song
 
 
 def band_list_view(request):
     result = Band.objects.all().values()
-
+    print(f"BANDS: {result}")
     return render(request, "band_list.html", {"bands": result})
 
 
 def band_detail_view(request, id):
     result = Band.objects.get(id=id)
-
-    return render(request, "band_detail.html", {"band": result})
+    print(f"BAND: {result}")
+    albums = Album.objects.filter(band=result)
+    print(f"ALbums: {albums}")
+    return render(request, "band_detail.html",
+                  {"band": result,
+                   "albums": albums})
 
 
 def band_create_view(request):
@@ -26,3 +30,11 @@ def band_create_view(request):
         return redirect("bands")
 
     return render(request, "band_create.html")
+
+
+def album_detail_view(request, id):
+    result = Album.objects.get(id=id)
+    songs = Song.objects.filter(album=result)
+    return render(request, "album_detail.html",
+                  {"album": result,
+                   "songs": songs})
