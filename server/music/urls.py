7f9decb46358916ce800a9e65459102f52233129
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-08-10 13:47:42 +0200

    band detial view

--- server/music/urls.py
+++ server/music/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import band_list_view
+from .views import band_list_view, band_detail_view
 
 
 urlpatterns = [
     path('list/', band_list_view, name="bands"),
+    path('detail/<int:id>', band_detail_view, name="b_detail"),
 ]
--- server/music/views.py
+++ server/music/views.py
@@ -6,3 +6,9 @@ def band_list_view(request):
     result = Band.objects.all().values()
 
     return render(request, "band_list.html", {"bands": result})
+
+
+def band_detail_view(request, id):
+    result = Band.objects.get(id=id)
+
+    return render(request, "band_detail.html", {"band": result})
