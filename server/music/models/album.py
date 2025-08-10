0252c220f44e22831fe06526b3f761269f1fabc5
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-08-10 15:00:56 +0200

    foreign key album

--- server/music/models/album.py
+++ server/music/models/album.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .band import Band
 
 
 class Album(models.Model):
@@ -15,3 +16,4 @@ class Album(models.Model):
     title = models.CharField(max_length=100)
     release_year = models.IntegerField(validators=[MinValueValidator(1800), MaxValueValidator(2100)])
     rating = models.IntegerField(choices=STARS)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
